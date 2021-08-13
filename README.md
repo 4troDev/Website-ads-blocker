aabe67f467e97dbe4f5606d7eb5172945978dbc4
MailyBot <voltdevelopment@yahoo.com>
2021-08-13 12:06:48 -0400

    .

---
+++ README.md
@@ -0,0 +1,7 @@
+Add the domains you will like to block in const defaultFilters this can be found in background.js line 2 
+
+Making any changes to manifest.json with out knowing what you are doing can cause problems and your ad blocker wont work
+
+How to install go to https://code.visualstudio.com/ and download it or use your preferable IDE (Integrated development environment) Code editor
+
+https://developer.chrome.com/docs/extensions/mv3/getstarted/
---
+++ background.js
@@ -0,0 +1,19 @@
+// add domains that you would like to block
+const defaultFilters = [ 
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
